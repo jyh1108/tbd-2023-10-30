1d47240e3e88a2a7608cb4e1a8c242ade7748571
jyh1108 <kobcom12@naver.com>
2023-10-30 09:50:21 +0900

    1단계

---
+++ src/main/java/com/ll/Calc.java
@@ -0,0 +1,7 @@
+package com.ll;
+
+class Calc {
+    static int run(String bb) {
+        return 0;
+    }
+}
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,15 +1,9 @@
 package com.ll;
 
-import jdk.internal.misc.InnocuousThread;
-
 public class Main {
     public static void main(String[] args) {
-        Calc.run("3 * 1") == 3;
-    }
-}
-class Calc{
-    String bb;
-    static void run(String bb){
-
+        System.out.println(
+                "결과 : " + (Calc.run("3 * 1")== 3)
+        );
     }
 }
